5ab29ee356ebcdd7ae8adc7cb7b1bb54ae5ea372
Daniel Watt <dano4owl@gmail.com>
2019-04-04 23:49:14 +1300

    added final refactor for open_off.cpp

--- open_off.cpp
+++ open_off.cpp
@@ -1,10 +1,51 @@
+/*
+ * Functions given by Dr. Mukundan and refactored by myself.
+ */
+
+#define GL_SILENCE_DEPRECATION
+
 #include <iostream>
 #include <fstream>
+#include <GL/freeglut.h>
 #include "open_off.h"
 
 using namespace std;
 
-void loadMeshFile(const char* fname, float* x[], float* y[], float* z[], int* t1[], int* t2[], int* t3[], int* num_verticies, int* num_triangles)
+void normal(int tindx, const float x[], const float y[], const float z[], const int t1[], const int t2[], const int t3[])
+/* Calculates the normal vector given x, y, z vertex arrays and t1, t2, 
+ * t3 index arrays. Calls glNormal3f() to put the normal vector into the
+ * gl pipeline. */
+{
+	float x1 = x[t1[tindx]], x2 = x[t2[tindx]], x3 = x[t3[tindx]];
+	float y1 = y[t1[tindx]], y2 = y[t2[tindx]], y3 = y[t3[tindx]];
+	float z1 = z[t1[tindx]], z2 = z[t2[tindx]], z3 = z[t3[tindx]];
+	float nx, ny, nz;
+	nx = y1*(z2-z3) + y2*(z3-z1) + y3*(z1-z2);
+	ny = z1*(x2-x3) + z2*(x3-x1) + z3*(x1-x2);
+	nz = x1*(y2-y3) + x2*(y3-y1) + x3*(y1-y2);
+	glNormal3f(nx, ny, nz);
+}
+
+
+
+void draw_off_file(const float x[], const float y[], const float z[], const int t1[], const int t2[], const int t3[], int num_triangles)
+/* Draws an off_file imported with load_mesh_file into x, y, z, t1, t2, 
+ * t3 and num_triangles. */
+{
+    glBegin(GL_TRIANGLES);
+        for(int triangle_index = 0; triangle_index < num_triangles; triangle_index++)
+        {
+           normal(triangle_index, x, y, z, t1, t2, t3);
+           glVertex3d(x[t1[triangle_index]], y[t1[triangle_index]], z[t1[triangle_index]]);
+           glVertex3d(x[t2[triangle_index]], y[t2[triangle_index]], z[t2[triangle_index]]);
+           glVertex3d(x[t3[triangle_index]], y[t3[triangle_index]], z[t3[triangle_index]]);
+        }
+    glEnd();
+}
+
+
+
+void load_mesh_file(const char* fname, float* x[], float* y[], float* z[], int* t1[], int* t2[], int* t3[], int* num_verticies, int* num_triangles)
 /* Loads an OFF meshfile which specifies only triangles. 
  * The vertex data is loaded into 6 lists; x, y, z, t1, t2, t3. 
  * 
--- open_off.h
+++ open_off.h
@@ -1,12 +1,30 @@
+/*
+ * Functions given by Dr. Mukundan and refactored by myself.
+ */
+
 #ifndef OPEN_OFF_H
 #define OPEN_OFF_H 
 
 #include <iostream>
 #include <fstream>
+#include <GL/freeglut.h>
 
 using namespace std;
 
-void loadMeshFile(const char* fname, float* x[], float* y[], float* z[], int* t1[], int* t2[], int* t3[], int* num_verticies, int* num_triangles);
+void normal(int tindx, const float x[], const float y[], const float z[], const int t1[], const int t2[], const int t3[]);
+/* Calculates the normal vector given x, y, z vertex arrays and t1, t2, 
+ * t3 index arrays. Calls glNormal3f() to put the normal vector into the
+ * gl pipeline. */
+
+
+
+void draw_off_file(const float x[], const float y[], const float z[], const int t1[], const int t2[], const int t3[], int num_triangles);
+/* Draws an off_file imported with load_mesh_file into x, y, z, t1, t2, 
+ * t3 and num_triangles. */
+
+
+
+void load_mesh_file(const char* fname, float* x[], float* y[], float* z[], int* t1[], int* t2[], int* t3[], int* num_verticies, int* num_triangles);
 /* Loads an OFF meshfile which specifies only triangles. 
  * The vertex data is loaded into 6 lists; x, y, z, t1, t2, t3. 
  * 
